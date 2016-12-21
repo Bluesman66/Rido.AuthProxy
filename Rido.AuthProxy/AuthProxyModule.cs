c85d7bd632ffbfd9fcad487c573ff3d6a239c4c1
alex.paclin <alex.paclin@gmail.com>
2016-12-21 14:58:01 +0500

    create project

---
+++ Rido.AuthProxy/AuthProxyModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace Rido.AuthProxy
+{
+	public class AuthProxyModule : IWebProxy
+	{
+		private ICredentials _crendential = new NetworkCredential("user", "password");
+
+		public ICredentials Credentials
+		{
+			get
+			{
+				return _crendential;
+			}
+			set
+			{
+				_crendential = value;
+			}
+		}
+
+		public Uri GetProxy(Uri destination)
+		{
+			return new Uri("http://addr:port", UriKind.Absolute);
+		}
+
+		public bool IsBypassed(Uri host)
+		{
+			return host.IsLoopback;
+		}
+
+	}
+}
